567ab2388d586aaa732e921efe813bb1c32e3f8b
Jhohells Erick Dianderas Lopez <jhohellserick@gmail.com>
2024-12-18 07:47:44 -0500

    added SplashScreen.kt

---
+++ app/src/main/java/com/example/myapplication/ui/splash/SplashScreen.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication.ui.splash
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.myapplication.ui.home.RecetasScreen
+import kotlinx.coroutines.delay
+
+
+@Composable
+fun SplashScreen(navController: NavHostController) {
+    LaunchedEffect(Unit) {
+        delay(2000)
+        navController.navigate(RecetasScreen.Home.name)
+    }
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center) {
+        Text(text = "SPLASH SCREEN - Recetario APP")
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen(navController = rememberNavController())
+}
