d83f063694362fc1897d62a35ab0e15c7407c21a
Jhohells Erick Dianderas Lopez <jhohellserick@gmail.com>
2024-12-21 19:31:50 -0500

    separate viewmodel

---
+++ app/src/main/java/com/example/myapplication/ui/home/RecetasViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication.ui.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myapplication.domain.model.RecetaDomain
+import com.example.myapplication.domain.usecase.GetRecetasUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class RecetasViewModel(
+    private val getRecetasUseCase: GetRecetasUseCase
+) : ViewModel() {
+
+    private var _recetas: MutableStateFlow<List<RecetaDomain>> = MutableStateFlow(emptyList())
+    val recetas = _recetas.asStateFlow()
+
+    init {
+        loadRecetas()
+    }
+
+    private fun loadRecetas(){
+        viewModelScope.launch {
+            val recetas = getRecetasUseCase.execute()
+            _recetas.value = recetas
+        }
+    }
+}
